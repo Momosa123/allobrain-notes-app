c21cbd3637e85c8d5c260d8280ac764cbff1b738
Mouhamadou Sarr <mouhamadoul.sarr@gmail.com>
2025-04-14 14:31:45 +0200

    database init

--- backend/initial_data.py
+++ backend/initial_data.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 def init_db() -> None:
     logger.info("creating initial database tables...")
     # Create all defined tables that inherit from Base
-    Base.metadata.creat_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
     logger.info("Database tables created.")
 
 
